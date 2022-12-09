e8b4b69467baa188cff56d153442deeefe053c15
Zakhar Menshikov <zm681986@mail.ru>
2022-12-10 00:23:05 +0500

    ServicesMe

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1055165e26508c34"; }
+/******/ 		__webpack_require__.h = function() { return "22c8b95154dc2eb4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Footer.js
+++ components/Footer.js
@@ -1,51 +1,51 @@
-
 import Image from 'next/image';
+import styles from "../styles/Footer.module.css";
 function Footer() {
     return (
-        <footer className='footer'>
-            <h1 className='footer__title'>
+        <footer className={styles.footer}>
+            <h1 className={styles.footer__title}>
                 © 2022 Valeria Smolentseva
             </h1>
-            <div className='footer__info'>
-                <div className='footer__address'>
-                    <p className='footer__year'>
-                        <Image className='iqon__location' src='/images/location.png'
+            <div className={styles.footer__info}>
+                <div className={styles.footer__address}>
+                    <p className={styles.footer__year}>
+                        <Image className={styles.iqon__location} src='/images/location.png'
                             alt='logo'
                             width={150}
                             height={150}
                         />
                         Челябинск, Образцова 13</p>
-                    <p className='footer__year'>
-                        <Image className='iqon__location' src='/images/email.png'
+                    <p className={styles.footer__year}>
+                        <Image className={styles.iqon__location} src='/images/email.png'
                             alt='logo'
                             width={150}
                             height={150}
                         />
                         lera8787@inbox.ru</p>
                 </div>
-                <ul className='footer__list'>
+                <ul className={styles.footer__list}>
 
-                    <li className='footer__list-item'>
+                    <li className={styles.footer__list_item}>
                         <a href="tel:+79514840077">
-                            <Image className='iqon__logo' src='/images/phone_2.png'
+                            <Image className={styles.iqon__logo} src='/images/phone_2.png'
                                 alt='logo'
                                 width={150}
                                 height={150}
                             />
                         </a>
                     </li>
-                    <li className='footer__list-item'>
+                    <li className={styles.footer__list_item}>
                         <a href="https://wa.me/79514840077" target="_blank" rel="noreferrer">
-                            <Image className='iqon__logo' src='/images/whatsapp_2.svg'
+                            <Image className={styles.iqon__logo} src='/images/whatsapp_2.svg'
                                 alt='logo'
                                 width={150}
                                 height={150}
                             />
                         </a>
                     </li>
-                    <li className='footer__list-item'>
+                    <li className={styles.footer__list_item}>
                         <a href="https://instagram.com/valeria_colorist?igshid=YmMyMTA2M2Y=" target="_blank" rel="noreferrer">
-                            <Image className='iqon__logo' src='/images/instagram_2.svg'
+                            <Image className={styles.iqon__logo} src='/images/instagram_2.svg'
                                 alt='logo'
                                 width={150}
                                 height={150}
@@ -53,18 +53,18 @@ function Footer() {
                         </a>
                     </li>
 
-                    <li className='footer__list-item'>
+                    <li className={styles.footer__list_item}>
                         <a href="https://t.me/Valeria_Smolentseva" target="_blank" rel="noreferrer">
-                            <Image className='iqon__logo' src='/images/telegram_2.svg'
+                            <Image className={styles.iqon__logo} src='/images/telegram_2.svg'
                                 alt='logo'
                                 width={150}
                                 height={150}
                             />
                         </a>
                     </li>
-                    <li className='footer__list-item'>
+                    <li className={styles.footer__list_item}>
                         <a href="https://vk.com/public200598040" target="_blank" rel="noreferrer">
-                            <Image className='iqon__logo' src='/images/vk_2.svg'
+                            <Image className={styles.iqon__logo} src='/images/vk_2.svg'
                                 alt='logo'
                                 width={150}
                                 height={150}
